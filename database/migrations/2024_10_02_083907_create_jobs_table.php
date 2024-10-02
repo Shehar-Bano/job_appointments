edc561907585f24a197b211d87366f1d5fc6759f
Shehar-Bano <bshehar2002@gmail.com>
2024-10-02 14:02:20 +0500

    database added

---
+++ database/migrations/2024_10_02_083907_create_jobs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->enum('job_type', ["full_time","part_time"]);
+            $table->json('requirement');
+            $table->json('skills');
+            $table->enum('status', ["open","close"])->default('open');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
---
+++ database/migrations/2024_10_02_084409_create_appointment_forms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointment_forms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->integer('contact');
+            $table->longText('cover_letter');
+            $table->string('resume')->default('open');
+            $table->bigInteger('slot_id');
+            $table->foreign('slot_id')->references('id')->on('slots');
+            $table->date('date');
+            $table->bigInteger('job_id');
+            $table->foreign('job_id')->references('id')->on('jobs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointment_forms');
+    }
+};
