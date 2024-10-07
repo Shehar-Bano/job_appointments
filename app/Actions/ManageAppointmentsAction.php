c1902799c446b739ff77a979f29e66eeba4b2cca
Hirakhizar <hirakhizarkhizarhayat@gmail.com>
2024-10-07 03:42:05 -0700

    manage-appointments 10/06/2024

--- app/Actions/ManageAppointmentsAction.php
+++ app/Actions/ManageAppointmentsAction.php
@@ -1,7 +1,6 @@
 <?php
 namespace App\Actions;
 
-use App\Helpers\ResponseHelper;
 use App\Mail\AppointmentCancelled;
 use App\Models\AppointmentForm;
 use Illuminate\Support\Facades\Mail;
@@ -55,7 +54,7 @@ class ManageAppointmentsAction
             return false;
         }
         if($appointment->status == 'canceled'){
-            return ResponseHelper::error('You already canceled this appointment');
+            return false;
         }
         $appointment->status = 'canceled';
         $appointment->save();
